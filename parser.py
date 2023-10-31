fad80626319835ae7cc9b49ccac01b5e43868a0b
Vladimir Pilipenko <52373540+irving2019@users.noreply.github.com>
2023-10-31 22:54:39 +0500

    parser.py

--- parser.py
+++ parser.py
@@ -3,9 +3,9 @@ url = input("Enter your url: ") #view url = https(http)://site.domen
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
-file1 = open("Myfyle2.txt", "w")
+file1 = open("File.txt", "w")
 file1.write(html)
-with open("Myfyle2.txt", "w") as f:
+with open("File.txt", "w") as f:
     data = [html]
     line = ">"
     for line in data:
