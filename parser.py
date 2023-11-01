a8bb38052490d38619db0f35ccfde04647f9b7cc
Vladimir Pilipenko <52373540+irving2019@users.noreply.github.com>
2023-11-01 17:52:07 +0500

    parser.py

--- parser.py
+++ parser.py
@@ -1,7 +1,12 @@
 from urllib.request import urlopen
-url = input("Enter your url: ") #view url = https(http)://site.domen
+url = input("Введите url: ")
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
-file1 = open("File.txt", "w") # file for save
+file1 = open("index.html", "w")
 file1.write(html)
+with open("index.html", "w") as f:
+    data = [html]
+    line = ">"
+    for line in data:
+        f.write(line + "\n")
